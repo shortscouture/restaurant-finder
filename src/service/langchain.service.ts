4013d2521ff71046844e1c7e023333d0030f1a03
kpsantos <kpsantos@student.apc.edu.ph>
2025-11-24 02:16:21 +0800

    make export async so its callable to index.ts

--- src/service/langchain.service.ts
+++ src/service/langchain.service.ts
@@ -18,56 +18,60 @@ const config: Config = {
   openaiAPI: process.env.OPENAI_API_KEY,
   //fourSquareAPI: process.env.FOURSQUARE_API,
 };
-/*zod schema*/
-const RestaurantInfo = z.object({
-  query: z.string(),
-  address: z.string(),
-  taste: z.array(z.string),
-});
 
-/*LLM*/
-const model = new ChatOpenAI({
-  model: "gpt-3.5-turbo",
-  temperature: 0.1,
-  maxTokens: 1000,
-  timeout: 30,
-  apiKey: config.openaiAPI,
-});
 
-/*catch error*/
-const handleToolErrors = createMiddleWare({
-  name: "HandleToolErrors",
-  wrapToolCall: async (request, handler) => {
-    try {
-      return await handler(request);
-    } catch (error) {
-      // Return a custom error message to the model
-      return new ToolMessage({
-        content: `Tool error: Please check your input and try again. (${error})`,
-        tool_call_id: request.toolCall.id!,
-      });
-    }
-  },
-});
-/* agent */
+  /*zod schema*/
+  const RestaurantInfo = z.object({
+    query: z.string(),
+    address: z.string(),
+    taste: z.array(z.string),
+  });
 
-const agent = createAgent({
-  model,
-  responseFormat: RestaurantInfo, 
-  middleware: [handleToolErrors],
-});
+  /*LLM*/
+  const model = new ChatOpenAI({
+    model: "gpt-3.5-turbo",
+    temperature: 0.1,
+    maxTokens: 1000,
+    timeout: 30,
+    apiKey: config.openaiAPI,
+  });
 
-/*result from agent*/
-const result = await agent.invoke({
-    messages: [
-    {
-      role: "user",
-      content: "Find a sushi restaurant near me in Los Angeles ",
+  /*catch error*/
+  const handleToolErrors = createMiddleWare({
+    name: "HandleToolErrors",
+    wrapToolCall: async (request, handler) => {
+      try {
+        return await handler(request);
+      } catch (error) {
+        // Return a custom error message to the model
+        return new ToolMessage({
+          content: `Tool error: Please check your input and try again. (${error})`,
+          tool_call_id: request.toolCall.id!,
+        });
+      }
     },
-  ],
-});
+  });
+  /* agent */
 
+  const agent = createAgent({
+    model,
+    responseFormat: RestaurantInfo, 
+    middleware: [handleToolErrors],
+    systemPrompt: "You are an AI chatbot that will take the user input of the customer, asking about "
+  });
+
+  /*result from agent*/
+export async function agentResult () {
+  const result = await agent.invoke({
+      messages: [
+      {
+        role: "user",
+        content: "Find a sushi restaurant near me in Los Angeles ",
+      },
+    ],
+  });
+}
 /*log json*/
-console.log(result.structuredResponse); /*API request should capture this?*/
+console.log(agentResult.structuredResponse); /*API request should capture this?*/
 
 
