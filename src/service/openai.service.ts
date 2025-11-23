edbb2e0bcd114ae425f0aa90a0479c057a30bbb8
kpsantos <kpsantos@student.apc.edu.ph>
2025-11-24 04:45:55 +0800

    installed openai platform as an alternative to langchain

---
+++ src/service/openai.service.ts
@@ -0,0 +1,38 @@
+import dotenv from "dotenv";
+dotenv.config({ path:'/Users/shortscouture/Documents/js/restaurant-finder/.env' });
+import { OpenAI } from "openai";
+import { zodTextFormat } from "openai/helpers/zod";
+import {z} from "zod";
+interface Config {
+ // port: number
+  nodeEnv: string;  
+  openaiAPI: string;  
+  //fourSquareAPI: string;
+};
+/*env config*/
+const config: Config = {
+  //port: Number(process.env.PORT),
+  nodeEnv: process.env.NODE_ENV,
+  openaiAPI: process.env.OPENAI_API_KEY,
+  //fourSquareAPI: process.env.FOURSQUARE_API,
+}; 
+
+const client = new OpenAI();
+
+const RestaurantSchema = z.object({
+    query: z.string(),
+    address: z.string(),
+    taste: z.array(z.string()),
+  });
+
+const response = await client.responses.parse({
+    model: "gpt-3.5-turbo",
+    input: [
+    { role: "system", content: "Extract what the user asks of you when checking for restaurants." },
+    {role: "user", content: "I want to eat in a sushi restaurant near Los Angeles.",
+    },
+  ],
+  text: {
+    format: zodTextFormat(CalendarEvent, "event"),
+  },
+});
